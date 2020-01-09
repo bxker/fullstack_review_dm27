efdac3d2041f07802f581c872147d4ea44749bae
bxker <jacob@Jacobs-MacBook-Pro.local>
2020-01-09 11:28:21 -0600

    11:28

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
 
-const register = async () => {
+const register = async (req, res) => {
     const db = req.app.get('db')
     const {username, password, is_Admin} = req.body
 
@@ -12,12 +12,41 @@ const register = async () => {
         const salt = bcrypt.genSaltSync(10)
         const hash = bcrypt.hashSync(password, salt)
 
-        const newUser = db.registerUser(username, hash, is_Admin)
+        const newUser = await db.registerUser(username, hash, is_Admin)
+
+        console.log(newUser)
+
+        req.session.user = {
+            user_id: newUser[0].user_id,
+            username: newUser[0].username,
+            is_Admin: newUser[0].is_admin
+        }
+        res.status(200).json(req.session.user)
     }
 }
-// const login = () => {
+const login = async (req, res) => {
+    const db = req.app.get('db')
+    const {username, password} = req.body
 
-// }
+    const foundUser = await db.checkForUsername(username)
+
+    if(!foundUser[0]){
+        res.status(403).json('Please make an account')
+    }else{
+        const authUser = bcrypt.compareSync(password, foundUser[0].hash)
+
+        if(!authUser){
+            res.status(403).json('Username or Password Incorrect')
+        }else{
+            req.session.user = {
+                user_id: foundUser[0].user_id,
+                username: foundUser[0].username,
+                is_Admin: foundUser[0].is_admin
+            }
+            res.status(200).json(req.session.user)
+        }
+    }
+}
 // const logout = () => {
 
 // }
@@ -27,7 +56,7 @@ const register = async () => {
 
 module.exports = {
     register,
-    // login,
+    login,
     // logout,
     // getUser
 }
--- server/index.js
+++ server/index.js
@@ -3,14 +3,25 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const massive = require('massive')
+const session = require('express-session')
 
 //controllers
+const {register, login} = require('./controllers/userController');
 
 //dotenv
-const {SERVER_PORT, CONNECTION_STRING} = process.env
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
 
 //middleware
 app.use(express.json())
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000*60*60*24*7
+    }
+}))
 
 //massive
 massive(CONNECTION_STRING)
@@ -20,6 +31,9 @@ massive(CONNECTION_STRING)
 })
 
 //endpoints
+//auth
+app.post('/auth/register', register)
+app.post('/auth/login', login)
 
 //listen
 app.listen(SERVER_PORT, () => console.log(`Server listening on ${SERVER_PORT}`))
